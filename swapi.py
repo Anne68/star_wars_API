ccd75f30a0229eb490f4ac3388456a6019887be7
Guillaume Demerges <guillaume.demerges@protonmail.com>
2024-03-06 10:55:22 +0100

    ajout fonction pour selectionner un personnage

--- swapi.py
+++ swapi.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+from pandas import json_normalize
 
 def get_character(url):
     names = []
@@ -17,8 +18,24 @@ def get_character(url):
 def create_names_df(names):
     return pd.DataFrame(names, columns=['Name']) #creation du dataframe
 
+def character(nombre):
+    url = f'https://swapi.dev/api/people/{nombre}'
+    response = requests.get(url)
+    if response.status_code == 200: #200 équivaut à une requête réussie
+            print(response)
+            return response.json()
+    else:
+            print(f"Erreur lors de la récupération des données: {response.status_code}")
 
 url = 'https://swapi.dev/api/people/'
 names = get_character(url)
 names_df = create_names_df(names)
 print(names_df)
+
+response_data = character("4")
+if response_data:
+    df_character = json_normalize(response_data)
+    print("Personnage Star Wars :")
+    print(df_character)
+else:
+    print("Erreur lors de la récupération des informations.")
