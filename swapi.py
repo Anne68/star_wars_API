744c29a8fc3f20a5dd6d2796f75a1d1aff1b892f
Anne Atse <vicky69200@gmail.com>
2024-03-06 15:16:53 +0100

    Fonction qui récupère les planètes

--- swapi.py
+++ swapi.py
@@ -2,60 +2,31 @@ import requests
 import pandas as pd
 from pandas import json_normalize
 
-# fetch all the starships in the api based on the url in parameters
-def get_starships(url):
-    names = []
-    while url:
-        response = requests.get(url)
-        if response.status_code == 200:
-            starships = response.json()
-            names.extend([i['name'] for i in starships['results']])
-            url = starships['next']
-        else:
-            print(f"La requête a échoué avec le code d'état {response.status_code}")
-            break
-    return names
+def fetch_star_wars_data(nmb):
+    api_url = f"https://swapi.dev/api/planets/{nmb}"
+    response = requests.get(api_url)
 
-# create a dataframe from a list of starships (names)
-def create_starships_df(names):
-    return pd.DataFrame(names, columns=['Name'])
-
-# fetch the starship defined with the id in parameters
-def get_starship(nombre):
-    url = f'https://swapi.dev/api/starships/{nombre}/'
-    response = requests.get(url)
+    # Check if the request was successful (status code 200)
     if response.status_code == 200:
-        starships = response.json()
-        return starships
+        print(response)
+        # Check if 'results' key exists in the data
+        return response.json()
     else:
-        print("Impossible de récupérer les informations car la requête a échoué.")
+        print(f"Error: Failed to fetch data. Status Code: {response.status_code}")
         return None
 
+def fetch_planet_data():
+    api_url = "https://swapi.dev/api/planets/"
+    response = requests.get(api_url)
 
-#fonction qui récupère tout les noms des personnages de l'API
-def get_characters(url):
-    names = []
-    while url: #tant que l'url est valide et non null on reste dans le while
-        response = requests.get(url)
-        if response.status_code == 200: #200 équivaut à une requête réussie
-            data = response.json()
-            names.extend([i['name'] for i in data['results']]) #on ajoute à la liste names chaque valeur qui se situe dans les name(elles même situés dans results)
-            url = data['next'] #on affecte l'url qui se situe au endpoint next
-        else:
-            print(f"Erreur lors de la récupération des données: {response.status_code}")
-            break
-    return names
-
-#fonction pour créer le dataframe de tout les personnages
-def create_names_df(names):
-    return pd.DataFrame(names, columns=['Name'])
-
-#fonction qui récupère un personnage suivant l'identifiant qu'on spécifie
-def get_character(nombre):
-    url = f'https://swapi.dev/api/people/{nombre}'
-    response = requests.get(url)
-    if response.status_code == 200: #200 équivaut à une requête réussie
-            print(response)
-            return response.json()
+    if response.status_code == 200:
+        data = response.json()
+        # Extracting relevant information (planet name and size) from the API response
+        planet_data = [{'Name': planet['name'], 'Size': int(planet['diameter'])} for planet in data['results']]
+        return planet_data
     else:
-            print(f"Erreur lors de la récupération des données: {response.status_code}")
+        print(f"Error: Failed to fetch data. Status Code: {response.status_code}")
+        return None
+
+def display_planet_dataframe():
+    planet_data = fetch_planet_data()
